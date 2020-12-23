77f95ff8d3bd33ac682fc8a41a7b2f835c5e7189
diydriller <dhrhd080@naver.com>
2020-12-24 01:06:50 +0900

    Update CodeValidateRequest.java

--- app/src/main/java/com/example/application/CodeValidateRequest.java
+++ app/src/main/java/com/example/application/CodeValidateRequest.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class CodeValidateRequest extends StringRequest {
-    final static private String url="http://dhrhd080.cafe24.com/CodeValidate.php";
+    final static private String url="URL";
     private Map<String,String> parameters;
 
     public CodeValidateRequest(String userCode, Response.Listener<String> listener){
